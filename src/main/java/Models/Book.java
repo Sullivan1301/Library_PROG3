f57617a8b2d337217f22ff79ca07eda5a2cfef89
Sullivan13 <jorosullivan13@gmail.com>
2023-11-18 14:21:22 +0300

    create book sql

--- src/main/java/Models/Book.java
+++ src/main/java/Models/Book.java
@@ -1,1 +1,29 @@
-package Models;import lombok.*;import java.sql.Date;import java.time.LocalDate;@Getter@Setter@ToString@AllArgsConstructor@NoArgsConstructor@EqualsAndHashCodepublic class Book {   private String id;   private String bookName;   private int pageNumbers;   private Topic topic;   private LocalDate releaseDate;   private Author author;   private String status;   enum Topic{      COMEDY,      ROMANCE,      OTHER   }}
+package Models;
+
+import lombok.*;
+
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+
+public class Book {
+   private String id;
+   private String bookName;
+   private int pageNumbers;
+   private Topic topic;
+   private LocalDate releaseDate;
+   private int authorID;
+   private String status;
+
+   enum Topic{
+      COMEDY,
+      ROMANCE,
+      OTHER
+   }
+}
---
+++ src/main/java/Repositories/create_table_book.sql
@@ -0,0 +1,17 @@
+package Repositories;
+
+CREATE TABLE IF NOT EXISTS book (
+    id serial PRIMARY KEY,
+    nameBook VARCHAR(255) NOT NULL,
+    pageNumbers int NOT NULL,
+    topic VARCHAR(20) DEFAULT 'OTHER' CHECK (topic IN('COMEDY','ROMANCE','OTHER' )),
+    releaseDate DATE,
+    author VARCHAR (255),
+    status VARCHAR(50),
+
+    CONSTRAINT fkAuthor Foreign Key (author) REFERENCES author(name)
+);
+
+INSERT INTO book VALUES ('1', 'Naruto', 90, 'COMEDY', '2023-11-16', 1, 'AVAILABLE'),
+    ('2', 'Kanojo no Kanojo', 120, 'ROMANCE', '2023-11-14', 2, 'BORROWED'),
+    ('3', 'Atomic Habits', 200, 'OTHER', '2023-11-17', 3, 'AVAILABLE');
